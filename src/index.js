19f2f91e5332e0de3603733de512aacd041805ca
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-07-21 12:42:55 +0300

    DONE

--- src/index.js
+++ src/index.js
@@ -1,10 +1,84 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter,NavLink, Route, Switch} from "react-router-dom"
 
 function Home() {
   return (
     <div>
-      <h1>Home!</h1>
+    <h1>Home!</h1>
+  </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+    </div>
+  );
+}
+
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
@@ -13,4 +87,17 @@ function App() {
   return <Home />;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( <BrowserRouter>
+<Switch>
+   <NavBar />
+  <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route exact path="/">
+  <Home />
+  </Route>
+    </Switch>
+  </BrowserRouter>, document.getElementById("root"));
